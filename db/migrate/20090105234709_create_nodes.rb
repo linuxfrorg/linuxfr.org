f5ef2e11768f31fcd7c7ea864c081242b3bf9e4b
Bruno Michel <bmichel@menfin.info>
2009-01-06 00:48:57 +0100

    new model: node

---
+++ db/migrate/20090105234709_create_nodes.rb
@@ -0,0 +1,15 @@
+class CreateNodes < ActiveRecord::Migration
+  def self.up
+    create_table :nodes do |t|
+      t.integer :score
+      t.string :content_type
+      t.integer :content_id
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :nodes
+  end
+end
