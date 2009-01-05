a0126a585e85053d2ca285e899013036d882e771
Bruno Michel <bmichel@menfin.info>
2009-01-06 00:42:41 +0100

    New resource: user

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+# Schema version: 20090105233501
+#
+# Table name: users
+#
+#  id         :integer(4)      not null, primary key
+#  login      :string(255)
+#  email      :string(255)
+#  name       :string(255)
+#  homesite   :string(255)
+#  jabber_id  :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+end
