adac155778f902ec09191e03518e0e40d22cb233
Bruno Michel <bmichel@menfin.info>
2009-01-25 23:55:25 +0100

    Use config.gem

--- config/environment.rb
+++ config/environment.rb
@@ -19,11 +19,11 @@ Rails::Initializer.run do |config|
   # Specify gems that this application depends on. 
   # They can then be installed with "rake gems:install" on new installations.
   # You have to specify the :lib option for libraries, where the Gem name (sqlite3-ruby) differs from the file itself (sqlite3)
-  # gem 'will-paginate'
-  # gem 'ruby-openid'
-  # gem 'rubyist-aasm'
-  # gem 'mysql'
-  # gem 'mislav-will_paginate', '~>2.1'
+  config.gem 'mysql', :version => '~>2.7'
+  config.gem 'haml', :version => '~>2.0'
+  config.gem 'WikiCreole', :lib => 'wiki_creole', :version => '~>0.1'
+  config.gem 'rubyist-aasm', :lib => 'aasm', :version => '~>2.0'
+  config.gem 'mislav-will_paginate', :lib => 'will_paginate', :version => '~>2.1'
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
@@ -67,7 +67,3 @@ Rails::Initializer.run do |config|
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 end
 
-# FIXME config.gem doesn't seem to work in rails edge
-require 'will_paginate'
-require 'aasm'
-require 'wiki_creole'
