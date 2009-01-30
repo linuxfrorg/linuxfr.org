034ca28804fb74acf9a21d2b63e392e039d195b0
Bruno Michel <bmichel@menfin.info>
2009-01-30 23:23:51 +0100

    If an user has voted on a node, we cancel his vote before he votes again on it

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -14,18 +14,32 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :node
 
-  validates_uniqueness_of :node_id, :scope => :user_id
-
-  # TODO we should check if the user has already vote, and increment/decrement twice in that case
-
+  # An user can vote for a node...
   def self.for(user, node)
+    cancel(user, node)
     user.votes.create(:node_id => node.id, :vote => true)
     Node.increment_counter(:score, node.id)
   end
 
+  # ...or he can vote against it
   def self.against(user, node)
+    cancel(user, node)
     user.votes.create(:node_id => node.id, :vote => false)
     Node.decrement_counter(:score, node.id)
+  end
+
+protected
+
+  # Cancel a previous vote
+  def self.cancel(user, node)
+    vote = user.votes.first(:conditions => {:node_id => node.id})
+    return if vote.nil?
+    if vote.vote
+      Node.decrement_counter(:score, node.id)
+    else
+      Node.increment_counter(:score, node.id)
+    end
+    vote.destroy
   end
 
 end
