010ba9ef0e91493a4a35f1851fe9af4942d549a8
Bruno Michel <bmichel@menfin.info>
2009-02-25 22:49:54 +0100

    Oops, the last commit broke some links

--- config/routes.rb
+++ config/routes.rb
@@ -84,5 +84,5 @@ ActionController::Routing::Routes.draw do |map|
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 
-  map.connect ':action', :controller => 'static'
+  map.static ':action', :controller => 'static'
 end
