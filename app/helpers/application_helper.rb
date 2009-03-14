8cf40935fbeaa25c9b1d3f0cbaa816dd8ae18c45
Bruno Michel <bmichel@menfin.info>
2009-03-14 22:11:12 +0100

    Added some classes on <body>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,6 +18,17 @@ module ApplicationHelper
     @keywords += tags
   end
 
+  def body_attr
+    classes = %w(js-off)
+    classes << current_user.role if current_user && current_user.amr?
+    classes << Rails.env if Rails.env != 'production'
+    { :class => classes.join(' ') }
+  end
+
+  def check_js
+    javascript_tag "document.body.className = document.body.className.replace('js-off', 'js-on');"
+  end
+
   def admin_only(&blk)
     blk.call if current_user && current_user.admin?
   end
