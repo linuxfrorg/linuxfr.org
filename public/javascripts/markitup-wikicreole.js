721f1a89b6b10f7402684befb3edf91537552dab
Bruno Michel <bmichel@menfin.info>
2009-03-15 14:09:35 +0100

    Thanks Chtitux for his improvement requests

--- public/javascripts/markitup-wikicreole.js
+++ public/javascripts/markitup-wikicreole.js
@@ -14,10 +14,10 @@ var markItUpSettings = {
   onShiftEnter: {keepDefault: false, replaceWith: '\\\\'},
   onCtrlEnter: {keepDefault: false, replaceWith: '\n\n'},
   markupSet: [
-    {name:'Heading 1', key:'1', openWith:'==', closeWith:'==', className:'h1', placeHolder:'Your title here...' },
-    {name:'Heading 2', key:'2', openWith:'===', closeWith:'===', className:'h2', placeHolder:'Your title here...' },
-    {name:'Heading 3', key:'3', openWith:'====', closeWith:'====', className:'h3', placeHolder:'Your title here...' },
-    {name:'Heading 4', key:'4', openWith:'=====', closeWith:'=====', className:'h4', placeHolder:'Your title here...' },
+    {name:'Heading 1', key:'1', openWith:'== ', closeWith:' ==', className:'h1', placeHolder:'Your title here...' },
+    {name:'Heading 2', key:'2', openWith:'=== ', closeWith:' ===', className:'h2', placeHolder:'Your title here...' },
+    {name:'Heading 3', key:'3', openWith:'==== ', closeWith:' ====', className:'h3', placeHolder:'Your title here...' },
+    {name:'Heading 4', key:'4', openWith:'===== ', closeWith:' =====', className:'h4', placeHolder:'Your title here...' },
     {separator:'---------------' },
     {name:'Bold', key:'B', openWith:'**', closeWith:'**', className:'bold', placeHolder:'Your text here...'},
     {name:'Italic', key:'I', openWith:'//', closeWith:'//', className:'italic', placeHolder:'Your text here...'},
@@ -26,10 +26,10 @@ var markItUpSettings = {
     {name:'Numeric list', openWith:'# ', className:'list-numeric'},
     {separator:'---------------' },
     {name:'Picture', key:"P", replaceWith:'{{[![Url:!:http://]!]|[![Alternative text]!]}}', className:'image'},
-    {name:'Link', key:"L", openWith:"[", closeWith:'|[![Url:!:http://]!]|[![Title]!]]', className:'link', placeHolder:'Your text to link here...' },
+    {name:'Link', key:"L", replaceWith:'[[[![Url:!:http://]!]|[![Title]!]]]', className:'link'},
     {separator:'---------------' },
     {name:'Quotes', openWith:'> ', className:'quotes'},
-    {name:'Code block', openWith:'{{{', closeWith:'}}}', className:'code'},
+    {name:'Code block', openWith:'{{{', closeWith:'}}}', className:'code'}
   ]
 };
 
