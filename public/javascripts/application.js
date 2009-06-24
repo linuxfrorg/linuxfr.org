64703d1b97f5ac90fbf7fa42289a9f65972d64fe
Bruno Michel <bmichel@menfin.info>
2009-06-24 02:02:23 +0200

    Show the toolbar, and navigate in new comments with it

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -23,18 +23,35 @@ Toolbar.go_to_current = function() {
     if (this.nb_comments == 0) return ;
     var comment = this.comments[this.current - 1];
     $(document).scrollTop($(comment).offset().top);
+	$('#current-comment').text(Toolbar.current);
 };
+
+/* Show the toolbar */
+$('body').append('<div id="toolbar"><span>Nouveaux commentaires : <span id="current-comment">' +
+		Toolbar.current + '</span> / ' + Toolbar.nb_comments +
+		' <a href="#" accesskey="<" class="prev">&lt;</a> | ' +
+		' <a href="#" accesskey=">" class="next">&gt;</a>' +
+		'</span></div>');
+
+$('#toolbar .prev').click(function() {
+    Toolbar.prev_comment();
+	return false;
+});
+$('#toolbar .next').click(function() {
+    Toolbar.next_comment();
+	return false;
+});
 
 /* Use the '<' and '>' to navigate in the new comments */
 $(document).keypress(function(e) {
     if ($(e.target).is("input")) return ;
     if (e.which == 60) {
         Toolbar.prev_comment();
-        e.preventDefault();
+		return false;
     }
     if (e.which == 62) {
         Toolbar.next_comment();
-        e.preventDefault();
+		return false;
     }
 });
 
