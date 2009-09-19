eb1db968f3015f92ee72f465ab9a7b9e64add679
Bruno Michel <bmichel@menfin.info>
2009-09-19 19:46:14 +0200

    Translate previous and next in will_paginate

---
+++ config/initializers/will_paginate.rb
@@ -0,0 +1,3 @@
+WillPaginate::ViewHelpers.pagination_options[:previous_label] = '&laquo; Précedent'
+WillPaginate::ViewHelpers.pagination_options[:next_label] = 'Suivant &raquo;'
+WillPaginate::ViewHelpers.pagination_options[:inner_window] = 10
