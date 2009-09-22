e75f5d9c44404d4fbee2cc938d0c0d33941bdcdd
Bruno Michel <bmichel@menfin.info>
2009-09-22 23:09:36 +0200

    More validations on account

--- app/models/account.rb
+++ app/models/account.rb
@@ -33,15 +33,15 @@ class Account < ActiveRecord::Base
 
 ### Validation ###
 
-  # TODO add more validation checks
-  validates_presence_of :login
-  validates_presence_of :email
+  validates_presence_of :login, :message => "Veuillez choisir un pseudo"
+  validates_presence_of :email, :message => "Veuillez entrer votre adresse email"
+  validates_uniqueness_of :email, :case_sensitive => false, :message => "Cette adresse email est déjà utilisée pour un compte LinuxFr.org"
 
 ### Authentication ###
 
   acts_as_authentic do |config|
-    config.validates_length_of_login_field_options :within => 3..30
-    config.validates_uniqueness_of_login_field_options :case_sensitive => true
+    config.validates_length_of_login_field_options :within => 3..30, :message => "Le login doit faire au moins 3 caractères, et pas plus de 30 caractères"
+    config.validates_uniqueness_of_login_field_options :case_sensitive => true, :message => "Ce login est déjà utilisé, veuillez en choisir un autre"
     config.perishable_token_valid_for 24.hours
   end
 
