e2a31fc4f81f642d334a8b61a2682b90d09b2141
Bruno Michel <bmichel@menfin.info>
2009-10-19 01:34:11 +0200

    New plugin: rails_xss

--- config/initializers/haml_config.rb
+++ config/initializers/haml_config.rb
@@ -3,7 +3,6 @@
 # See http://haml.hamptoncatlin.com/docs/rdoc/classes/Haml.html -> HAML Options
 #
 
-Haml::Template.options[:escape_html]  = true
 Haml::Template.options[:ugly]         = true
 Haml::Template.options[:format]       = :html5
 Haml::Template.options[:attr_wrapper] = '"'
