96a55bbc33f17a79a5617e729da61d4dad71adb8
Bruno Michel <bmichel@menfin.info>
2010-02-20 19:13:06 +0100

    Clean config/application.rb: do not load active_resource, remove comments...

--- config/application.rb
+++ config/application.rb
@@ -1,55 +1,26 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require 'rails'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
 
 # Auto-require default libraries and those for the current Rails environment.
 Bundler.require :default, Rails.env
 
 module LinuxfrOrg
   class Application < Rails::Application
-    # TODO Rails3
-    # config.frameworks -= [ :active_resource ]
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Add additional load paths for your own custom dirs
-    # config.load_paths += %W( #{config.root}/extras )
-
-    # Only load the plugins named here, in the order given (default is alphabetical).
-    # :all can be used as a placeholder for all plugins not explicitly named
-    # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
-    # Activate observers that should always be running
-    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
-    # config.i18n.default_locale = :de
-
-    # Configure generators values. Many other options are available, be sure to check the documentation.
     config.generators do |g|
       g.orm              :active_record
       g.template_engine  :haml
       g.integration_tool :rspec
-      g.test_framework   :rspec,
-                         :fixture_replacement => :factory_girl,
-                         :fixture     => false,
-                         :helpers     => false,
-                         :routes      => false,
-                         :integration => true
+      g.test_framework   :rspec, :fixture_replacement => :factory_girl
     end
 
     config.after_initialize do
       ActionView::Base.sanitized_allowed_attributes.merge %w(data-id data-after)
     end
 
-    # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters << :password << :password_confirmation
   end
 end
