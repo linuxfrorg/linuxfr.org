4c01a5c88d0d30248a71cd57b86e70cb3ec5b62f
Bruno Michel <bmichel@menfin.info>
2010-03-01 01:32:35 +0100

    Extract the nested fields as a jquery plugin

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,86 +1,55 @@
-/*global $, jQuery, markItUpSettings */
+/*global jQuery, markItUpSettings */
 
-$(".markItUp").markItUp(markItUpSettings);
+(function($) {
+    $(".markItUp").markItUp(markItUpSettings);
 
-$("a.hit-counter").each(function() {
-    this.href = "/redirect/" + $(this).attr('data-hit');
-});
+    $("a.hit-counter").each(function() {
+        this.href = "/redirect/" + $(this).attr('data-hit');
+    });
 
-/* Ready to moule */
-// TODO $('.board').board('.inbox');
-$("#main-board input[type=text]").select();
+    /* Ready to moule */
+    // TODO $('.board').board('.inbox');
+    $("#main-board input[type=text]").select();
 
-/* Animate the scrolling to a fragment */
-$("a.scroll").click(function() {
-    var dst = $(this.hash);
-    var pos = dst ? dst.offset().top : 0;
-    $('html').animate({scrollTop: pos}, 500);
-    return false;
-});
+    /* Animate the scrolling to a fragment */
+    $("a.scroll").click(function() {
+        var dst = $(this.hash);
+        var pos = dst ? dst.offset().top : 0;
+        $('html').animate({scrollTop: pos}, 500);
+        return false;
+    });
 
-$('input.autocomplete').each(function() {
-    var input = $(this);
-    input.autocomplete(input.attr('data-url'), {multiple: true, multipleSeparator: ' '});
-});
+    $('input.autocomplete').each(function() {
+        var input = $(this);
+        input.autocomplete(input.attr('data-url'), {multiple: true, multipleSeparator: ' '});
+    });
 
-/* Add/Remove dynamically links in the news form. */
-var FormLinks = {};
-FormLinks.div      = null;
-FormLinks.counter  = 0;
-FormLinks.template = '<fieldset class="link">' +
-'  <input id="news_links_attributes_{i}_title" name="news[links_attributes][{i}][title]" size="30" type="text" /> ' +
-'  <input id="news_links_attributes_{i}_url" name="news[links_attributes][{i}][url]" size="30" type="text" /> ' +
-'  <input id="news_links_attributes_{i}_lang" name="news[links_attributes][{i}][lang]" size="30" type="text" /> ' +
-'</fieldset>';
-FormLinks.create = function(div) {
-    var links = div.children('.link');
-    FormLinks.counter = links.length;
-    links.each(function() { FormLinks.bind_link(this); });
-    div.append('<fieldset><button type="button" id="add-link">Ajouter un lien</button></fieldset>');
-    $('#add-link').click(function() { FormLinks.add_link(div); });
-};
-FormLinks.bind_link = function(link) {
-    var l = $(link);
-    l.append('<button type="button" class="remove">Supprimer ce lien</button>');
-    l.children('.remove').click(function() { FormLinks.remove_link(l); });
-};
-FormLinks.add_link = function(div) {
-    var last_link = div.children('.link:last');
-    last_link.after($.nano(FormLinks.template, {i: FormLinks.counter}));
-    FormLinks.bind_link(last_link.next());
-    FormLinks.counter += 1;
-};
-FormLinks.remove_link = function(link) {
-    link.remove();
-};
+    /* Add/Remove dynamically links in the news form. */
+    $("#form-links").nested_fields("news", "link", "lien", {title: 'text', url: 'url', lang: 'text'});
+    // TODO lang should be a <select>, not an <input type="text">
 
-/* Create the button for adding/removing links */
-if ($('#form-links')) {
-    FormLinks.create($('#form-links'));
-}
-
-/* Show the toolbar */
-if ($('body').hasClass('logged')) {
-    if ($('#comments').length > 0) {
-        $('#comments .new-comment').toolbar('Nouveaux commentaires', {folding: '#comments .comment'});
+    /* Show the toolbar */
+    if ($('body').hasClass('logged')) {
+        if ($('#comments').length > 0) {
+            $('#comments .new-comment').toolbar('Nouveaux commentaires', {folding: '#comments .comment'});
+        }
+        if ($('#contents').length > 0) {
+            $('#contents .new-content').toolbar('Contenus pas encore visités');
+        }
     }
-    if ($('#contents').length > 0) {
-        $('#contents .new-content').toolbar('Contenus pas encore visités');
-    }
-}
 
-/* Hotkeys */
-$(document)
-.bind('keypress', 'g', function() {
-  $('html').animate({scrollTop: 0}, 500);
-  return false;
-})
-.bind('keypress', 'shift+g', function() {
-  $('html').animate({scrollTop: $('body').scrollHeight}, 500);
-  return false;
-})
-.bind('keypress', 'shift+?', function() {
-    // TODO complete help
-    $.noticeAdd({text: "Raccourcis clavier :<ul><li>? pour l'aide</li></ul>"});
-});
-
+    /* Hotkeys */
+    $(document)
+    .bind('keypress', 'g', function() {
+      $('html').animate({scrollTop: 0}, 500);
+      return false;
+    })
+    .bind('keypress', 'shift+g', function() {
+      $('html').animate({scrollTop: $('body').scrollHeight}, 500);
+      return false;
+    })
+    .bind('keypress', 'shift+?', function() {
+        // TODO complete help
+        $.noticeAdd({text: "Raccourcis clavier :<ul><li>? pour l'aide</li></ul>"});
+    });
+})(jQuery);
---
+++ public/javascripts/jquery.nested_fields.js
@@ -0,0 +1,58 @@
+/*global jQuery */
+
+(function($) {
+    $.NestedFields = function(el, parent, nested, text, attributes) {
+        var base = this;
+        base.element = $(el);
+
+        base.init = function() {
+            base.parent = parent;
+            base.nested = nested;
+            base.text = text;
+            base.attributes = attributes;
+            base.create();
+        };
+
+        base.create = function() {
+            var items = base.element.children('.' + base.nested);
+            base.counter = items.length;
+            items.each(function() { base.bind_item(this); });
+            base.element.append($('<fieldset/>', {html: $('<button/>', {
+                type: "button",
+                id: "add-" + base.nested,
+                text: "Ajouter un " + base.text
+            })}));
+            $('#add-' + base.nested).click(function() { base.add_item(); });
+        };
+
+        base.bind_item = function(item) {
+            var it = $(item);
+            it.append('<button type="button" class="remove">Supprimer ce ' + base.text + '</button>');
+            it.children('.remove').click(function() { base.remove_item(it); });
+        };
+
+        base.add_item = function() {
+            var last = base.element.children('.' + base.nested + ':last');
+            var fset = $('<fieldset/>', {"class": base.nested});
+            last.after(fset);
+            for (var i in base.attributes) {
+                var name = base.parent + '[' + base.nested + 's_attributes][' + base.counter + '][' + i + ']';
+                $('<input/>', {name: name, type: base.attributes[i]}).appendTo(fset);
+            }
+            base.bind_item(last.next());
+            base.counter += 1;
+        };
+
+        base.remove_item = function(item) {
+            item.remove();
+        };
+
+        base.init();
+    };
+
+    $.fn.nested_fields = function(parent, nested, text, attributes) {
+        return this.each(function() {
+            (new $.NestedFields(this, parent, nested, text, attributes));
+        });
+    };
+})(jQuery);
