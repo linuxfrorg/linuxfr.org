b343b71c59c6519539f63a382a9676e968d69a04
Bruno Michel <bmichel@menfin.info>
2010-06-30 17:24:54 +0200

    Upgrade rspec

--- Gemfile
+++ Gemfile
@@ -35,7 +35,7 @@ group :development do
 end
 
 group :test do
-  gem "rspec-rails",           ">=2.0.0.beta.14"
+  gem "rspec-rails",           ">=2.0.0.beta.15"
   gem "factory_girl_rails",    "~>1.0"
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,13 +3,10 @@ require File.dirname(__FILE__) + "/../config/environment" unless defined?(Rails)
 require 'rspec/rails'
 require 'factory_girl'
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
-
 RSpec.configure do |c|
   c.mock_with :rspec
-  #c.filter_run :focus => true  
+  c.filter_run :focus => true
+  c.run_all_when_everything_filtered = true
 end
 
 class ActionController::TestCase
