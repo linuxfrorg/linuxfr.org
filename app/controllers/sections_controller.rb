26849adc3579e76f489f56fca403d134735d8c35
Bruno Michel <bmichel@menfin.info>
2010-10-23 20:07:51 +0200

    Add a box with the listing of sections

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -5,9 +5,10 @@ class SectionsController < ApplicationController
   end
 
   def show
-    @order   = params[:order] || 'created_at'
-    @section = Section.find(params[:id])
-    @news    = @section.news.with_node_ordered_by(@order).paginate(:page => params[:page], :per_page => 10)
+    @sections = Section.published
+    @order    = params[:order] || 'created_at'
+    @section  = Section.find(params[:id])
+    @news     = @section.news.with_node_ordered_by(@order).paginate(:page => params[:page], :per_page => 10)
     respond_to do |wants|
       wants.html
       wants.atom
