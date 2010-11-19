ecb823097b6e3ca6ddf07c055a9faec163f1331c
Bruno Michel <bmichel@menfin.info>
2010-11-19 21:12:29 +0100

    Mark validation errors as HTML-safe

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -7,7 +7,7 @@ module FormHelper
       content_tag(:ul) do
         object.errors.values.flatten.map do |err|
           content_tag(:li, err)
-        end.join
+        end.join.html_safe
       end
     end
   end
