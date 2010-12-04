4cd8b81e190622d39e74ed05d13ab102745ff493
Bruno Michel <bmichel@menfin.info>
2010-12-05 00:27:30 +0100

    Redirect to the current page when the user logs in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,9 @@ class SessionsController < ApplicationController
     cookies.permanent[:https] = { :value => "1", :secure => false } if request.ssl?
     @account = warden.authenticate!(:scope => :account, :recall => "new")
     sign_in :account, @account
-    redirect_to stored_location_for(:account) || "/", :notice => I18n.t("devise.sessions.signed_in")
+    redirect_to stored_location_for(:account) || :back, :notice => I18n.t("devise.sessions.signed_in")
+  rescue ActionController::RedirectBackError
+    redirect_to '/'
   end
 
   # GET /account/sign_out
