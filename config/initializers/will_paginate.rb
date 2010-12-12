5ab5a6ac95280fdb597c3f20d3693b4597e8ee17
Bruno Michel <bmichel@menfin.info>
2010-12-13 00:13:48 +0100

    Typo

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -1,4 +1,4 @@
 # encoding: UTF-8
-WillPaginate::ViewHelpers.pagination_options[:previous_label] = '&laquo; Précedent'
+WillPaginate::ViewHelpers.pagination_options[:previous_label] = '&laquo; Précédent'
 WillPaginate::ViewHelpers.pagination_options[:next_label]     = 'Suivant &raquo;'
 WillPaginate::ViewHelpers.pagination_options[:inner_window]   = 10
