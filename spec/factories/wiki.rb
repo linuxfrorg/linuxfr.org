2091d95742360d4e4a72df15a566f5a404bdb64e
Bruno Michel <bmichel@menfin.info>
2010-12-31 01:28:27 +0100

    Fix wiki specs

--- spec/factories/wiki.rb
+++ spec/factories/wiki.rb
@@ -1,6 +1,6 @@
 Factory.define :wiki, :class => WikiPage do |f|
   f.title "Historique de LinuxFr.org"
-  f.wiki_body "Quelques liens :\n\n* [[Templeet]]\n* [[PierreTramo]]\n"
+  f.wiki_body "Quelques liens :\n\n* [[[Templeet]]]\n* [[[PierreTramo]]]\n"
   f.owner_id 1
   f.message "Draft"
 end
