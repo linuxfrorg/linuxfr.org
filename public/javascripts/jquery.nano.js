0a714ce275cc093ea1d9351dd54d88e86525238c
Bruno Michel <bmichel@menfin.info>
2011-01-09 14:07:41 +0100

    Add a missing semi-colon in JS (bug with closure compiler)

--- public/javascripts/jquery.nano.js
+++ public/javascripts/jquery.nano.js
@@ -3,9 +3,9 @@
 (function($){
   $.nano = function(template, data){
     return template.replace(/\{([\w\.]*)}/g, function(str, key){
-      var keys = key.split("."), value = data[keys.shift()]
-      $.each(keys, function(){ value = value[this] })
-      return value
-    })
-  }
-})(jQuery)
+      var keys = key.split("."), value = data[keys.shift()];
+      $.each(keys, function(){ value = value[this]; });
+      return value;
+    });
+  };
+})(jQuery);
