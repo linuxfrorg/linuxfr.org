87ce701d3d4fdccd7aa9d64f7d17030bbdefc077
Bruno Michel <bmichel@menfin.info>
2011-01-09 01:05:55 +0100

    HTML cleanup

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -1,15 +1,11 @@
 module LayoutHelper
 
   def body_attr
-    classes = %w(js-off)
+    classes = []
     classes << 'logged' if current_account
     classes << current_account.role if current_account
     classes << Rails.env if Rails.env != 'production'
     { :class => classes.join(' '), :id => "#{controller.controller_name}-#{controller.action_name}" }
-  end
-
-  def check_js
-    javascript_tag "document.body.className = document.body.className.replace('js-off', 'js-on');"
   end
 
   def logo
