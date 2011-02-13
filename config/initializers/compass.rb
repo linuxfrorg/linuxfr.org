5e68c9205c5706fff1e066943be75a62ded7851c
Bruno Michel <bmichel@menfin.info>
2011-02-13 23:34:28 +0100

    Import RonRonnement as the new default CSS

--- config/initializers/compass.rb
+++
@@ -1,4 +0,0 @@
-require 'compass'
-Compass.add_project_configuration(File.join(Rails.root, "config", "compass.rb"))
-Compass.configure_sass_plugin!
-Compass.handle_configuration_change!
---
+++ config/initializers/sass.rb
@@ -0,0 +1,2 @@
+Sass::Plugin.remove_template_location("./public/stylesheets/sass")
+Sass::Plugin.add_template_location("./app/stylesheets")
