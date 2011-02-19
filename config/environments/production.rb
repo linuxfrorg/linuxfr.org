695201d08092a3843a82482bdece0eb58aad0db0
Bruno Michel <bmichel@menfin.info>
2011-02-19 23:33:14 +0100

    No newline for the hostname

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ LinuxfrOrg::Application.configure do
   # config.logger = SyslogLogger.new
 
   # Use a different cache store in production
-  config.cache_store = :redis_store, "redis://#{`hostname`}:6379/1/cache"
+  config.cache_store = :redis_store, "redis://#{`hostname`.chomp}:6379/1/cache"
 
   # Set the page cache directory
   config.action_controller.page_cache_directory = "#{Rails.public_path}/pages"
