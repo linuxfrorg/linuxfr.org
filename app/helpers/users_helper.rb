fabdb9183c96fcdf1b4d622b448cbfcdf4795dc1
Bruno Michel <bmichel@menfin.info>
2011-02-20 15:21:59 +0100

    help

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
   def avatar_url(user)
     return '' if user.nil?
     url = user.avatar.url
-    options = { :class => "avatar", :alt => "Avatar de #{user.name}", :width => AvatarUploader::AVATAR_SIZE, :heigth => AvatarUploader::AVATAR_SIZE }
+    options = { :class => "avatar", :alt => "Avatar de #{user.name}", :width => AvatarUploader::AVATAR_SIZE, :height => AvatarUploader::AVATAR_SIZE }
     if user.avatar.blank?
       options['data-gravatar'] = user.gravatar_hash
       url.sub!(/^http:/, 'https:') if request.ssl?
