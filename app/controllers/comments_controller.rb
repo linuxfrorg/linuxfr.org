521acb928ff86cf293a514c1385d62a5e00dac87
Bruno Michel <bmichel@menfin.info>
2011-02-22 00:19:13 +0100

    Show the note when previewing a future comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,6 +19,7 @@ class CommentsController < ApplicationController
   def new
     @comment = @node.comments.build
     enforce_create_permission(@comment)
+    @comment.default_score
   rescue Canable::Transgression
     flash[:alert] = "Impossible de commenter un contenu vieux de plus de 3 mois"
     redirect_to_content @node.content
