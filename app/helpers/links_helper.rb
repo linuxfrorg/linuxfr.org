321591460ef067a9e62ae949628dd1e6f1eb280e
Bruno Michel <bmichel@menfin.info>
2011-03-01 00:07:40 +0100

    Show flags for news in the moderation space

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -3,6 +3,7 @@ module LinksHelper
   def link_attr(link)
     ret = {}
     ret['data-url'] = edit_redaction_link_path(link)
+    ret[:lang] = link.lang
     ret[:class] = 'locked' if link.locked?
     ret
   end
