9280eaccfac4021c01bacf2a8b45281c1f195cdb
Bruno Michel <bmichel@menfin.info>
2011-03-28 20:46:16 +0200

    Fix the antispam when lots of empty links

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -49,7 +49,7 @@ protected
   def honeypot
     honeypot = params[:news].delete(:pot_de_miel)
     links = params[:news][:links_attributes].values
-    same  = links.group_by {|l| l["title"] }.map {|_,v| v.size }.max
+    same  = links.map {|l| l["title"] }.reject(&:blank?).group_by(&:to_s).map {|_,v| v.size }.max.to_i
     render :nothing => true if honeypot.present? || same >= 3
   end
 
