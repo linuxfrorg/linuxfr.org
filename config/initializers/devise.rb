aef39b7de30a534811bf3770f120f026c4753b9c
Bruno Michel <bmichel@menfin.info>
2011-08-23 00:21:13 +0200

    Use POST for logout

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -166,7 +166,7 @@ Devise.setup do |config|
   # config.navigational_formats = [:"*/*", "*/*", :html]
 
   # The default HTTP method used to sign out a resource. Default is :get.
-  # config.sign_out_via = :get
+  config.sign_out_via = :post
 
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
