9a5b08c69676a94c1dbf1837617683c6ad8c9bfb
Bruno Michel <bmichel@menfin.info>
2011-08-22 23:56:53 +0200

    Fix a bug when converting a diary in news

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -68,7 +68,7 @@ class DiariesController < ApplicationController
     @news.author_name  = @diary.owner.try(:name)
     @news.author_email = @diary.owner.try(:account).try(:email)
     if @news.save
-      @news.node.update_attribute(:cc_licenced => true) if @diary.node.cc_licensed?
+      @news.node.update_attribute(:cc_licenced, true) if @diary.node.cc_licensed?
       @news.links.create :title => "Journal à l'origine de la dépêche", :url => "#{MY_DOMAIN}/users/#{@diary.owner.to_param}/journaux/#{@diary.to_param}", :lang => "fr"
       @news.submit!
       if current_account.amr?
