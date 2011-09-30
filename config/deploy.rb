24b1ac99d57030047db3cce5acb5d8af1ed69d8b
Bruno Michel <bmichel@menfin.info>
2011-09-30 22:25:56 +0200

    Fix some other bugs with Rails 3.1 assets pipeline

--- config/deploy.rb
+++ config/deploy.rb
@@ -101,7 +101,7 @@ end
 namespace :assets do
   desc "Create a symlink for application.css (used by static pages)"
   task :static, :roles => :web, :except => { :no_release => true } do
-    %w(application.css).each do |asset|
+    %w(application.css grayscale.css spasibo.css spasibo-mobile.css).each do |asset|
       file = capture "cd #{shared_path}/assets && ruby -ryaml -e 'p YAML.load_file(\"manifest.yml\")[\"#{asset}\"]'"
       run "cd #{shared_path}/assets && ln -sf #{file.chomp} #{asset}"
     end
