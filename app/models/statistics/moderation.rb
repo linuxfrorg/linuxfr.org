9144968a2bbc369f829ceb949e2bb76db878d0f6
Bruno Michel <bmichel@menfin.info>
2011-11-20 18:11:45 +0100

    CSS for stats

--- app/models/statistics/moderation.rb
+++ app/models/statistics/moderation.rb
@@ -1,15 +1,44 @@
 class Statistics::Moderation < Statistics::Statistics
 
   def by_day
-    select_all "SELECT DAYNAME(CONVERT_TZ(created_at,'+00:00','Europe/Paris')) AS d, WEEKDAY(CONVERT_TZ(created_at,'+00:00','Europe/Paris')) AS day, COUNT(*) AS cnt FROM nodes WHERE content_type='News' GROUP BY d ORDER BY day ASC;"
+    select_all <<-EOS
+       SELECT DAYNAME(CONVERT_TZ(created_at,'+00:00','Europe/Paris')) AS d,
+              WEEKDAY(CONVERT_TZ(created_at,'+00:00','Europe/Paris')) AS day,
+              COUNT(*) AS cnt
+         FROM nodes
+        WHERE content_type='News'
+     GROUP BY d
+     ORDER BY day ASC
+    EOS
   end
 
   def average_time
-    select_all "SELECT YEAR(CONVERT_TZ(nodes.created_at,'+00:00','Europe/Paris')) AS year, COUNT(*) AS cnt, SUM(TIMESTAMPDIFF(SECOND,news.created_at,nodes.created_at)) AS duration, MIN(TIMESTAMPDIFF(SECOND,news.created_at,nodes.created_at)) AS min, MAX(TIMESTAMPDIFF(SECOND,news.created_at,nodes.created_at)) AS max FROM nodes, news WHERE nodes.content_id = news.id AND nodes.content_type='News' AND nodes.created_at >= DATE('2011-01-01 00:00:001') ORDER BY year;"
+    select_all <<-EOS
+       SELECT YEAR(CONVERT_TZ(nodes.created_at,'+00:00','Europe/Paris')) AS year,
+              COUNT(*) AS cnt,
+              SUM(TIMESTAMPDIFF(SECOND,news.created_at,nodes.created_at)) AS duration,
+              MIN(TIMESTAMPDIFF(SECOND,news.created_at,nodes.created_at)) AS min,
+              MAX(TIMESTAMPDIFF(SECOND,news.created_at,nodes.created_at)) AS max
+         FROM nodes, news
+        WHERE nodes.content_id = news.id
+          AND nodes.content_type='News'
+          AND nodes.created_at >= DATE('2011-01-01 00:00:001')
+     ORDER BY year;"
+    EOS
   end
 
   def top_amr(sql_criterion="")
-    select_all "SELECT login, moderator_id, COUNT(*) AS cnt FROM nodes,news,accounts WHERE moderator_id IS NOT NULL AND content_id=news.id AND content_type='News' AND moderator_id=accounts.user_id #{sql_criterion} GROUP BY moderator_id ORDER BY LOWER(login) ASC;"
+    select_all <<-EOS
+       SELECT login, moderator_id, COUNT(*) AS cnt
+         FROM nodes,news,accounts
+        WHERE moderator_id IS NOT NULL
+          AND content_id=news.id
+          AND content_type='News'
+          AND moderator_id=accounts.user_id
+          #{sql_criterion}
+    GROUP BY moderator_id
+    ORDER BY LOWER(login) ASC
+    EOS
   end
 
   def top_am
@@ -17,7 +46,7 @@ class Statistics::Moderation < Statistics::Statistics
   end
 
   def top_am_last_month
-    select_all <<EOS
+    select_all <<-EOS
       SELECT login, user_id, cnt
         FROM accounts
    LEFT JOIN (
@@ -29,7 +58,7 @@ class Statistics::Moderation < Statistics::Statistics
              ) AS j ON moderator_id = accounts.user_id
        WHERE (role='moderator' OR role='admin')
     ORDER BY LOWER(login) ASC
-EOS
+    EOS
   end
 
   def nb_votes(login)
