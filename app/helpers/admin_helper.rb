86eb88b74cddf900eecd3dfbcdf9ce328e6eb414
Bruno Michel <bmichel@menfin.info>
2012-07-27 20:20:35 +0200

    CSS

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -4,7 +4,7 @@ module AdminHelper
   def abusers
     file = Rails.root.join("tmp", "abusers.txt")
     if File.exists?(file)
-      content_tag(:ul) do
+      content_tag(:ul, :class => "abusers") do
         File.readlines(file).map do |l|
           content_tag(:li, l)
         end.join.html_safe
