3198d1a284ca9ce2859220339ab7fc921d2eb029
Bruno Michel <bmichel@menfin.info>
2012-11-01 19:10:14 +0100

    Don't generate useless stuff

--- config/application.rb
+++ config/application.rb
@@ -1,24 +1,22 @@
 # encoding: utf-8
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require 'rails'
 require 'active_record/railtie'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
-require "sprockets/railtie"
+require 'sprockets/railtie'
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require *Rails.groups(:assets => %w(development test))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
-end
+Bundler.require *Rails.groups(:assets => %w(development test))
 
 module LinuxfrOrg
   class Application < Rails::Application
     config.generators do |g|
       g.template_engine  :haml
       g.test_framework   :rspec, :fixture_replacement => :factory_girl
+      g.view_specs       false
+      g.helper           false
+      g.assets           false
     end
 
     config.after_initialize do
