aa0b6812bf0573ee119d8f2c7449774282743b89
Bruno Michel <bmichel@menfin.info>
2013-01-04 22:19:08 +0100

    Use a better lib for diffs

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -5,4 +5,18 @@ module ContentHelper
     HTML_Spellchecker.french.spellcheck(html).html_safe
   end
 
+  def htmldiff(was, new)
+    dmp = DiffMatchPatch.new
+    diff = dmp.diff_main(was, new, false)
+    dmp.diff_cleanup_semantic! diff
+    diff.map do |(op,txt)|
+      txt.force_encoding(Encoding::UTF_8)
+      case op
+      when  0  then h txt
+      when -1  then %Q{<del class="diff">#{h txt}</del>}
+      when  1  then %Q{<ins class="diff">#{h txt}</ins>}
+      end
+    end.join.html_safe
+  end
+
 end
