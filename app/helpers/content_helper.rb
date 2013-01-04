4d24eb41026121cfe31f482679e0d32d04a01cb4
Bruno Michel <bmichel@menfin.info>
2013-01-04 23:45:00 +0100

    Fix encoding issues

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -11,10 +11,7 @@ module ContentHelper
     dmp.diff_cleanup_semantic! diff
     diff.map do |(op,txt)|
       txt.force_encoding(Encoding::UTF_8)
-      unless txt.valid_encoding?
-        txt.force_encoding(Encoding::ISO8859_1)
-        txt = txt.encode(Encoding::UTF_8)
-      end
+      txt.encode!(Encoding::UTF_8, Encoding::UTF_8) unless txt.valid_encoding?
       case op
       when  0  then h txt
       when -1  then %Q{<del class="diff">#{h txt}</del>}
