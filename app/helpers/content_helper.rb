125905ca6fa087dbaa0528f49c450f6951ece05e
Bruno Michel <bmichel@menfin.info>
2013-01-08 23:35:58 +0100

    Use yet another lib for diff, pure Ruby to avoid encoding issue

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -6,16 +6,14 @@ module ContentHelper
   end
 
   def htmldiff(was, new)
-    dmp = DiffMatchPatch.new
-    diff = dmp.diff_main(was, new, false)
-    dmp.diff_cleanup_semantic! diff
-    diff.map do |(op,txt)|
-      txt.force_encoding(Encoding::UTF_8)
-      txt.encode!(Encoding::UTF_8, Encoding::UTF_8) unless txt.valid_encoding?
-      case op
-      when  0  then h txt
-      when -1  then %Q{<del class="diff">#{h txt}</del>}
-      when  1  then %Q{<ins class="diff">#{h txt}</ins>}
+    dmp = DiffPatchMatch.new
+    diffs = dmp.diff_main(was, new)
+    dmp.diff_cleanupSemantic diffs
+    diffs.map do |diff|
+      case diff.op
+      when :equal   then h diff.text
+      when :delete  then %Q{<del class="diff">#{h diff.text}</del>}
+      when :insert  then %Q{<ins class="diff">#{h diff.text}</ins>}
       end
     end.join.html_safe
   end
