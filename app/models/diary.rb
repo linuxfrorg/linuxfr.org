6a468185b6d9eaf6834429d1a00ee98daf999ca7
Bruno Michel <bmichel@menfin.info>
2013-01-21 23:30:28 +0100

    Diaries converted to news go to the redaction space if licensed CC by-sa

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -78,7 +78,7 @@ class Diary < Content
       $redis.set "convert/#{@news.id}", self.id
       @news.node.update_column(:cc_licensed, true) if node.cc_licensed?
       @news.links.create :title => "Journal à l'origine de la dépêche", :url => "#{MY_DOMAIN}/users/#{owner.to_param}/journaux/#{to_param}", :lang => "fr"
-      @news.submit!
+      @news.submit! unless node.cc_licensed?
       @news
     else
       nil
