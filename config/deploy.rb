63868235a03a1bc3bd7314003595f55987816601
Bruno Michel <bmichel@menfin.info>
2013-07-02 23:32:14 +0200

    Make plee_the_bear the default CSS

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,7 @@ desc "Production"
 task :prod do
   set :vserver,   "prod"
   set :user,      "linuxfr"
-  set :branch,    "master"
+  set :branch,    "plee_the_bear"
   set :rails_env, "production"
 end
 
