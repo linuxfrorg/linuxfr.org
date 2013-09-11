3bd4ded56aead8eeb89168d1c668fd08b9d917c6
Bruno Michel <bmichel@menfin.info>
2013-09-11 22:21:45 +0200

    Don't block the access to the redaction space to users with a negative karma

--- app/controllers/redaction_controller.rb
+++ app/controllers/redaction_controller.rb
@@ -6,7 +6,6 @@ class RedactionController < ApplicationController
   def index
     @boards = Board.all(Board.writing)
     @board  = @boards.build
-    enforce_view_permission(@board)
     @drafts = News.draft.sorted
     @news   = News.candidate.sorted
   end
