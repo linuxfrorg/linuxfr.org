9d958d800551484cec5facb0196d47352d4401c3
Bruno Michel <bmichel@menfin.info>
2013-11-06 22:04:04 +0100

    Upgarde devise

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 secrets = YAML.load_file(Rails.root.join('config/secret.yml'))
 
-Push.secret   = secrets['board_secret']
-Devise.pepper = secrets['devise_pepper']
+Push.secret       = secrets['board_secret']
+Devise.pepper     = secrets['devise_pepper']
+Devise.secret_key = secrets['devise_secret']
 Rails.application.config.secret_token = secrets['rails_secret']
