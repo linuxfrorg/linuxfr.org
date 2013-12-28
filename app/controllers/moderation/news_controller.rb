b52dfb7284dd17e8efb6ca6fdd77be80ea285344
Bruno Michel <bmichel@menfin.info>
2013-12-28 22:30:57 +0100

    Change the order of refused news in moderation

--- app/controllers/moderation/news_controller.rb
+++ app/controllers/moderation/news_controller.rb
@@ -7,7 +7,7 @@ class Moderation::NewsController < ModerationController
   def index
     @news    = News.candidate.sorted
     @drafts  = News.draft.sorted
-    @refused = News.refused.sorted.limit(15)
+    @refused = News.refused.order("updated_at DESC").limit(15)
     @polls   = Poll.draft
     @boards  = Board.all(Board.amr)
   end
