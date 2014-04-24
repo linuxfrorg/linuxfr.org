d6a17947919932a8656fca433932f40af31a26b0
Bruno Michel <bmichel@menfin.info>
2014-04-24 22:22:21 +0200

    Don't block iframes

--- config/environments/alpha.rb
+++ config/environments/alpha.rb
@@ -44,6 +44,12 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
+  # Remove X-Frame-Options header
+  config.action_dispatch.default_headers = {
+    'X-XSS-Protection' => '1; mode=block',
+    'X-Content-Type-Options' => 'nosniff'
+  }
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
