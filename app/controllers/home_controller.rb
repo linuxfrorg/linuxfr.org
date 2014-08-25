cc09ebd5beae5f0b18bbff3d4b985bd1125db531
Bruno Michel <bmichel@menfin.info>
2014-08-25 21:22:54 +0200

    Show the sections on the sidebar on the homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
   DEFAULT_TYPES = %w(News Poll)
 
   def index
+    @sections = Section.published
     @types  = current_account.try(:types_on_home)
     @types  = DEFAULT_TYPES if @types.blank?
     default = current_account.try(:sort_by_date_on_home) ? "created_at" : "interest"
