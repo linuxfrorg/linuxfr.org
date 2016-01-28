154251443bd5dbfe63afb6b9fd31b279fe48f736
Bruno Michel <bmichel@menfin.info>
2016-01-28 08:31:14 +0100

    Fix redaction and moderation after security fix

--- lib/resolvers.rb
+++ lib/resolvers.rb
@@ -4,8 +4,8 @@ class RedactionResolver < ::ActionView::FileSystemResolver
     super("app/views")
   end
 
-  def find_templates(name, prefix, partial, details)
-    super(name, prefix.sub("moderation", "redaction"), partial, details)
+  def find_templates(name, prefix, partial, details, outside_app_allowed = false)
+    super(name, prefix.sub("moderation", "redaction"), partial, details, outside_app_allowed)
   end
 end
 
@@ -14,7 +14,7 @@ class NoNamespaceResolver < ::ActionView::FileSystemResolver
     super("app/views")
   end
 
-  def find_templates(name, prefix, partial, details)
-    super(name, prefix.gsub(/^\w+\//, ""), partial, details)
+  def find_templates(name, prefix, partial, details, outside_app_allowed = false)
+    super(name, prefix.gsub(/^\w+\//, ""), partial, details, outside_app_allowed)
   end
 end
