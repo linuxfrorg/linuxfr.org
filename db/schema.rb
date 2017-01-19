c6ab0fff2a78be70566f19e8d9a28abf63ed8a0e
Bruno Michel <bmichel@menfin.info>
2017-01-19 21:04:18 +0100

    Fix collation for login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161104094116) do
+ActiveRecord::Schema.define(version: 20170119194958) do
 
   create_table "accounts", force: :cascade do |t|
     t.integer  "user_id",                limit: 4
