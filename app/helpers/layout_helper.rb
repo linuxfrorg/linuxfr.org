a59f0b70ddd04caba7287b02a0edb984743c366d
Obsidian <obsid@free.fr>
2017-12-28 09:45:09 +0100

    Adding "moderation_page" class to related pages's <body>
    This patch adds a class named "moderation_page" among the
    existing ones to the <body> tag of pages which controller
    path begins with "moderation", making them easier to spot
    by CSSes.
    
    It's not only convenient (it would widely simplify the CSS
    code) but it's also necessary on pages such as #news-index
    and #polls-index, which can't be discriminated from the
    outside to know if they're the common user pages or the
    moderation ones.
    
    As regards #news-index in particular, the two pages are
    totally different. The moderation page contains everything
    but article boxes, and also offers special services such as
    a chat board. The rest is formed of plain text and html
    links that thus should be displayed on a light-background
    page, not a dark one.

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -5,6 +5,7 @@ module LayoutHelper
     classes = []
     classes << 'logged' if current_account
     classes << current_account.role if current_account
+    classes << "moderation_page" if controller.controller_path.starts_with?('moderation')
     { class: classes.join(' '), id: "#{controller.controller_name}-#{controller.action_name}" }
   end
 
