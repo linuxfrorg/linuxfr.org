df20fac1824cfdba6a2fc01b7054c9ac8e485191
Bruno Michel <bmichel@menfin.info>
2018-01-24 09:44:01 +0100

    Fix for anonymous

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -5,7 +5,7 @@ module CommentsHelper
     classes = %w(comment)
     classes << "score#{comment.bound_score}"
     classes << "new-comment" unless comment.read_by?(current_account)
-    if v = comment.vote_by?(current_account.id)
+    if current_account && v = comment.vote_by?(current_account.id)
       classes << "vote-#{v}"
     end
     classes << cycle("odd", "even", name: "comment-#{comment.parent_id}")
