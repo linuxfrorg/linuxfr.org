957ab7b99ce1cae5baa0c4eb3ac9ea85df615eb5
Bruno Michel <bmichel@menfin.info>
2018-02-26 18:38:32 +0100

    Revert "Deploy from the before-redesign branch"
    This reverts commit 15bbbbbfe03c4a0e29feab8d9ae6c35113eb8dff.

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,7 @@ desc "Production"
 task :prod do
   set :vserver,   "prod"
   set :user,      "linuxfr"
-  set :branch,    "before-redesign"
+  set :branch,    "master"
   set :rails_env, "production"
 end
 
