d5d7ab5045923223dcb58e5125910b5882fa7a09
Bruno Michel <bmichel@menfin.info>
2018-03-25 14:49:09 +0200

    Make stats about OAuth apps more explicit

--- app/models/statistics/applications.rb
+++ app/models/statistics/applications.rb
@@ -9,6 +9,14 @@ class Statistics::Applications < Statistics::Statistics
     count("SELECT COUNT(DISTINCT(owner_id)) AS cnt FROM oauth_applications", "cnt").to_i
   end
 
+  def access_grants
+    count("SELECT COUNT(*) AS cnt FROM oauth_access_grants", "cnt").to_i
+  end
+
+  def access_grants_distinct_applications
+    count("SELECT COUNT(DISTINCT(application_id)) AS cnt FROM oauth_access_grants", "cnt").to_i
+  end
+
   def access_tokens
     return @access_tokens if @access_tokens
 
@@ -24,21 +32,6 @@ class Statistics::Applications < Statistics::Statistics
     count("SELECT COUNT(DISTINCT(application_id)) AS cnt FROM oauth_access_tokens", "cnt").to_i
   end
 
-  def access_grants
-    return @access_grants if @access_grants
-
-    @access_grants = { "0" => 0, "1" => 0, "total" => 0 }
-    by_state = select_all("SELECT COUNT(*) AS cnt, (created_at+expires_in<now() OR IFNULL(revoked_at,now()+'1 year') < now()) AS expired_revoked FROM oauth_access_grants GROUP BY expired_revoked")
-    by_state.each { |access_grant| @access_grants[access_grant["expired_revoked"]] = access_grant["cnt"] }
-    @access_grants["total"] = @access_grants.values.sum
-
-    @access_grants
-  end
-
-  def access_grants_distinct_applications
-    count("SELECT COUNT(DISTINCT(application_id)) AS cnt FROM oauth_access_grants", "cnt").to_i
-  end
-
   def applications_by_year
     return @applications_by_year if @applications_by_year
 
@@ -51,32 +44,30 @@ class Statistics::Applications < Statistics::Statistics
     @applications_by_year
   end
 
-  def access_tokens_by_year
-    return @access_tokens_by_year if @access_tokens_by_year
-
-    @access_tokens_by_year = {}
-    entries = select_all("SELECT YEAR(oauth_access_tokens.created_at) AS year, (created_at+expires_in<now() OR IFNULL(revoked_at,now()+'1 year') < now()) AS expired_revoked, COUNT(*) AS cnt FROM oauth_access_tokens GROUP BY year, expired_revoked ORDER BY year, expired_revoked")
-    entries.each do |entry|
-      @access_tokens_by_year[entry["year"]] ||= {}
-      @access_tokens_by_year[entry["year"]][entry["expired_revoked"]] ||= 0
-      @access_tokens_by_year[entry["year"]][entry["expired_revoked"]] += entry["cnt"]
-    end
-
-    @access_tokens_by_year
-  end
-
   def access_grants_by_year
     return @access_grants_by_year if @access_grants_by_year
 
     @access_grants_by_year = {}
-    entries = select_all("SELECT YEAR(oauth_access_grants.created_at) AS year, (created_at+expires_in<now() OR IFNULL(revoked_at,now()+'1 year') < now()) AS expired_revoked, COUNT(*) AS cnt FROM oauth_access_grants GROUP BY year, expired_revoked ORDER BY year, expired_revoked")
+    entries = select_all("SELECT YEAR(oauth_access_grants.created_at) AS year, COUNT(*) AS cnt FROM oauth_access_grants GROUP BY year ORDER BY year")
     entries.each do |entry|
-      @access_grants_by_year[entry["year"]] ||= {}
-      @access_grants_by_year[entry["year"]][entry["expired_revoked"]] ||= 0
-      @access_grants_by_year[entry["year"]][entry["expired_revoked"]] += entry["cnt"]
+      @access_grants_by_year[entry["year"]] = entry["cnt"]
     end
 
     @access_grants_by_year
+  end
+
+  def access_tokens_by_year
+    return @access_tokens_by_year if @access_tokens_by_year
+
+    @access_tokens_by_year = {}
+    entries = select_all("SELECT YEAR(oauth_access_tokens.created_at) AS year, IFNULL(revoked_at,now()+'1 year') < now() AS expired, COUNT(*) AS cnt FROM oauth_access_tokens GROUP BY year, expired ORDER BY year, expired")
+    entries.each do |entry|
+      @access_tokens_by_year[entry["year"]] ||= {}
+      @access_tokens_by_year[entry["year"]][entry["expired"]] ||= 0
+      @access_tokens_by_year[entry["year"]][entry["expired"]] += entry["cnt"]
+    end
+
+    @access_tokens_by_year
   end
 
 end
