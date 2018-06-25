b2dc2d3f80a4f21cbc239734a7157df7c67cb0bb
Bruno Michel <bmichel@menfin.info>
2018-06-25 22:42:49 +0200

    Fix search and tracker listing

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
 
   def index
+    params.permit!
     redirect_to "https://duckduckgo.com/?#{params.slice(:q).to_query}+site%3Alinuxfr.org"
   end
 
--- app/controllers/trackers_controller.rb
+++ app/controllers/trackers_controller.rb
@@ -7,7 +7,8 @@ class TrackersController < ApplicationController
   respond_to :html, :md
 
   def index
-    @attrs    = {"state" => "opened"}.merge(params[:tracker] || {})
+    @attrs    = params.require(:tracker).permit(:state, :category_id, :assigned_to_user_id)
+    @attrs    = {"state" => "opened"}.merge @attrs
     @order    = params[:order]
     @order    = "created_at" unless VALID_ORDERS.include?(@order)
     @trackers = Tracker.joins(:node).merge(Node.visible)
