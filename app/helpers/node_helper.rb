686cea844a1e9a9a5f43dc3e6707fafe116adabf
Bruno Michel <bmichel@menfin.info>
2018-07-20 20:08:26 +0200

    Add style for preview

--- app/helpers/node_helper.rb
+++ app/helpers/node_helper.rb
@@ -30,6 +30,7 @@ module NodeHelper
     cp.css_class << record.class.name.downcase
     cp.css_class << 'new-node' if current_account && record.node.read_status(current_account) == :not_read
     cp.css_class << 'ppp' if record.node.on_ppp?
+    cp.css_class << "preview" if @preview_mode
     yield cp
     cp.meta ||= posted_by(record)
     cp.tags ||= tags_for(record.node)
