2f03a8fc7e1c6e9097f7cc9849e673124366536d
Bruno Michel <bmichel@menfin.info>
2018-07-23 19:53:04 +0200

    Add a page about friend sites

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -6,11 +6,11 @@ module StaticHelper
 
   def people_list(accounts)
     content_tag(:ul, class: "people-list") do
-      accounts.map do |a|
+      safe_join(accounts.map do |a|
         content_tag(:li) do
           link_to avatar_img(a.user) + a.user.name, a.user
         end
-      end.join.html_safe
+      end)
     end
   end
 
@@ -26,8 +26,13 @@ module StaticHelper
     people_list Account.moderator
   end
 
+  def helper_friend_sites_list
+    sites = FriendSite.all.map { |s| content_tag(:li, link_to(s.title, s.url)) }
+    content_tag(:ul, safe_join(sites), class: "people-list")
+  end
+
   def helper_responses_list
     responses = Response.all.map { |r| content_tag(:li, content_tag(:pre, r.content)) }
-    content_tag(:ul, responses.join.html_safe)
+    content_tag(:ul, safe_join(responses))
   end
 end
