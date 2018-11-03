5e9f47d18963c31626601758e43ee3ec0b17510d
Benoît Sibaud <oumph@linuxfr.org>
2018-11-03 15:27:33 +0100

    Missing route for statistiques/applications

--- config/routes.rb
+++ config/routes.rb
@@ -213,7 +213,7 @@ Rails.application.routes.draw do
   get "/recherche(/:type(/:facet))" => "search#index", as: :search
 
   # Statistics
-  %i(tracker prizes users top moderation redaction contents comments tags).each do |action|
+  %i(tracker prizes users top moderation redaction contents comments tags applications).each do |action|
     get "/statistiques/#{action}", controller: :statistics, action: action
   end
 
