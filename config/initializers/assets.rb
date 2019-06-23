aa6d00e1c0ff00bbae0fde82e2ca41a87864d126
Adrien Dorsaz <adrien@adorsaz.ch>
2019-06-23 23:54:53 +0200

    redaction/news: remove scissors as they are useless now

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(sorttable.js)
-Rails.application.config.assets.precompile += %w(feather-icons/dist/icons/scissors.svg)
 Dir.chdir(Rails.root.join "app/assets/stylesheets") do
   Rails.application.config.assets.precompile += Dir["contrib/*"].map {|s| s.sub '.scss', '.css' }
   Rails.application.config.assets.precompile += Dir["common/*"].map {|s| s.sub '.scss', '.css' }
