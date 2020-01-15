44386483ddfe07929c892b808091a7b621318c17
Davy Defaud <davy.defaud@free.fr>
2020-01-15 19:19:35 +0100

    Orthotypographic fixes

--- app/helpers/atom_helper.rb
+++ app/helpers/atom_helper.rb
@@ -4,9 +4,9 @@ module AtomHelper
   def atom_comments_link(content, url)
     str = <<-EOS
     <p>
-      <strong>Commentaires :</strong>
-      <a href=\"//#{MY_DOMAIN}/nodes/#{content.node.id}/comments.atom\">voir le flux atom</a>
-      <a href=\"#{url}#comments\">ouvrir dans le navigateur</a>
+      <strong>Commentaires :</strong>
+      <a href=\"//#{MY_DOMAIN}/nodes/#{content.node.id}/comments.atom\">voir le flux Atom</a>
+      <a href=\"#{url}#comments\">ouvrir dans le navigateur</a>
     </p>
     EOS
     str.html_safe
