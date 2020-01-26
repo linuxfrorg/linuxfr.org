9f3873fa3e5bce78cc0f73185c9ba1a0489fc747
Davy Defaud <davy.defaud@free.fr>
2020-01-26 22:18:57 +0100

    Sort languages by names, responses by titles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,25 +5,25 @@ Logo.image = '/images/logos/linuxfr2_classic.png'
 
 # Langs
 Lang['fr'] = 'Français'
-Lang['en'] = 'Anglais'
 Lang['de'] = 'Allemand'
-Lang['it'] = 'Italien'
-Lang['es'] = 'Espagnol'
-Lang['fi'] = 'Finnois'
+Lang['en'] = 'Anglais'
 Lang['eu'] = 'Basque'
-Lang['ja'] = 'Japonais'
-Lang['ru'] = 'Russe'
-Lang['pt'] = 'Portugais'
-Lang['nl'] = 'Néerlandais'
-Lang['da'] = 'Danois'
-Lang['el'] = 'Grec'
-Lang['sv'] = 'Suédois'
-Lang['cn'] = 'Chinois'
-Lang['pl'] = 'Polonais'
 Lang['ct'] = 'Catalan'
-Lang['no'] = 'Norvégien'
+Lang['cn'] = 'Chinois'
 Lang['ko'] = 'Coréen'
+Lang['da'] = 'Danois'
+Lang['es'] = 'Espagnol'
 Lang['ee'] = 'Estonien'
+Lang['fi'] = 'Finnois'
+Lang['el'] = 'Grec'
+Lang['it'] = 'Italien'
+Lang['ja'] = 'Japonais'
+Lang['nl'] = 'Néerlandais'
+Lang['no'] = 'Norvégien'
+Lang['pl'] = 'Polonais'
+Lang['pt'] = 'Portugais'
+Lang['ru'] = 'Russe'
+Lang['sv'] = 'Suédois'
 Lang['xx'] = '!? hmmm ?!'
 Lang['wq'] = 'Code/binaire'
 
@@ -38,9 +38,9 @@ Response.create!(title: "Dépêche trop courte",         content: "La dépêche 
 Response.create!(title: "Dépêche déjà publiée",        content: "Le sujet de la dépêche que vous avez proposée a déjà fait l’objet d’une\ndépêche publiée sur LinuxFr.org. Pour cette raison, votre dépêche a été\nrefusée.")
 Response.create!(title: "Dépêche déjà proposée",       content: "Une ou plusieurs autres dépêches sur le même sujet a (ont) déjà été\nproposée(s) par un (ou plusieurs) autre(s) internaute(s). Dès qu’un\nmodérateur pourra s’en occuper, il utilisera celle qui lui semble la\nmieux rédigée.")
 Response.create!(title: "Dépêche hors sujet",          content: "La dépêche que vous avez envoyée est hors sujet par rapport au site. Si\nLinuxFr.org valide parfois les dépêches hors sujet, celle‑ci n’a pas\nretenu l’attention des modérateurs.")
+Response.create!(title: "Problèmes de rédaction",      content: "Cette dépêche pose des problèmes de rédaction, et il nous est difficile\nde la valider telle quelle. Pourriez‑vous re‑rédiger cette dépêche s’il\nvous plaît ?")
 Response.create!(title: "Redirection forum",           content: "Une entrée dans un forum serait plus adaptée pour poser cette question.\nNous tenons à vous remercier de l’avoir proposée, et nous\nvous encourageons à la poser dans un des forums du site.\n\nhttps://linuxfr.org/posts/nouveau")
 Response.create!(title: "Redirection journal",         content: "Votre journal est peut‑être plus adapté pour passer cette information.\n\nhttps://linuxfr.org//journaux/nouveau")
-Response.create!(title: "Problèmes de rédaction",      content: "Cette dépêche pose des problèmes de rédaction, et il nous est difficile\nde la valider telle quelle. Pourriez‑vous re‑rédiger cette dépêche s’il\nvous plaît ?")
 Response.create!(title: "Site (presque) vide",         content: "LinuxFr.org préfère valider des dépêches de ce type quand le site est\ndéjà bien avancé. Si tel est le cas dans quelque temps, n’hésitez pas\nà proposer une nouvelle dépêche.")
 Response.create!(title: "Version mineure du logiciel", content: "Votre dépêche traite d’une version mineure d’un logiciel, et n’apporte\nrien de fort d’un point de vue information. Pour cette raison, elle a été\nrefusée.")
 
