c6940d2561dddebfee3232a506c4984e9a4572ce
Adrien Dorsaz <adrien@adorsaz.ch>
2020-12-11 00:45:26 +0100

    redaction: split draft list according to update activity

--- app/controllers/redaction_controller.rb
+++ app/controllers/redaction_controller.rb
@@ -3,8 +3,10 @@ class RedactionController < ApplicationController
   append_view_path NoNamespaceResolver.new
 
   def index
+    activity_threshold = Date.today - 3.month
     @boards = Board.limit(25, Board.writing)
-    @drafts = News.draft.sorted
+    @drafts = News.draft.where("updated_at >= ?", activity_threshold).sorted
+    @inactive_drafts = News.draft.where("updated_at < ?", activity_threshold).sorted
     @news   = News.candidate.sorted
     @stats  = Statistics::Redaction.new
   end
