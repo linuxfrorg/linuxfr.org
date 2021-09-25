94381ffbd2fd8c0e51b5c0fe3b86b11a9a87a10a
Adrien Dorsaz <adrien@adorsaz.ch>
2021-09-25 22:31:19 +0200

    keep chosen license when a diary is converted to a news
    This was the original intention of commit 6a8984b526dbb81f785e5c61d6435edf47902ac2
    on March 2011. At that moment, the default value for `cc_licensed` on nodes
    where `false` and we only needed to set it to true if the diary
    license where the CC one.
    
    Although, on December 2012, we changed the default value to `false` with
    commit 7c6058f512bf004305eea2655d50bd1802b084e9 and it broke the diary
    converter algorithm.
    
    This fix allows to be not any more linked to the current default value
    on the `cc_licensed` node as we always enfoce the new news to have the
    same license.

--- test/models/diary_test.rb
+++ test/models/diary_test.rb
@@ -69,9 +69,8 @@ class DiaryTest < ActiveSupport::TestCase
     assert_equal diary.wiki_body, news.versions.first().second_part;
     assert_equal diary.owner.try(:name), news.author_name;
     assert_equal diary.owner.try(:account).try(:email), news.author_email;
-    # Even if original diary were copyrighted, the created news is cc_licensed
-    assert_not_equal diary.node.cc_licensed, news.node.cc_licensed;
-    assert news.node.cc_licensed;
+    assert_equal diary.node.cc_licensed, news.node.cc_licensed;
+    assert_not news.node.cc_licensed;
     assert_equal sections(:default).id, news.section_id;
     # As diary is not cc_licensed, news cannot be reworked collectively in the redaction space
     assert_equal "candidate", news.state;
