cebe4e130414e44c715b31784c61cef418036020
Benoît Sibaud <oumph@linuxfr.org>
2023-05-29 17:26:14 +0200

    Be able to un-plonk and un-block an account
    Includes current state on account and last accounts pages.
    Includes database log, board log and notification

--- app/controllers/moderation/block_controller.rb
+++ app/controllers/moderation/block_controller.rb
@@ -6,7 +6,12 @@ class Moderation::BlockController < ModerationController
     nb_days  = (params[:nb_days] || 2).to_i
     @account = Account.find(params[:account_id])
     @account.block(nb_days, current_user.id)
-    Board.amr_notification("Les commentaires de #{@account.login} #{user_url @account.login} ont été bloqués pendant #{pluralize nb_days, "jour"} par #{current_user.name} #{user_url(current_user)}")
-    render json: { notice: "Les commentaires de #{@account.login} sont bloqués pendant #{pluralize nb_days, "jour"}" }
+    if nb_days > 0
+      Board.amr_notification("Les commentaires de #{@account.login} #{user_url @account.login} ont été bloqués pendant #{pluralize nb_days, "jour"} par #{current_user.name} #{user_url(current_user)}")
+      render json: { notice: "Les commentaires de #{@account.login} sont bloqués pendant #{pluralize nb_days, "jour"}" }
+    else
+      Board.amr_notification("Les commentaires de #{@account.login} #{user_url @account.login} ont été débloqués par #{current_user.name} #{user_url(current_user)}")
+      render json: { notice: "Les commentaires de #{@account.login} sont débloqués" }
+    end
   end
 end
