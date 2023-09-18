ce67febd9abda0d907d93d77e23770c39a99578c
echarp <manu@echarp.org>
2023-09-18 09:14:12 +0200

    Mécanisme de résolution standard pour templates rédaction/modération
    Il y avait deux classes pour dynamiquement chercher des templates entre
    les espaces de rédaction et modération, après tests et recherches il
    semble que ça ne soit pas (ou plus?) nécessaire.

--- app/controllers/moderation_controller.rb
+++ app/controllers/moderation_controller.rb
@@ -1,8 +1,6 @@
 # encoding: utf-8
 class ModerationController < ApplicationController
   before_action :amr_required
-  append_view_path RedactionResolver.new
-  append_view_path NoNamespaceResolver.new
 
   def index
     redirect_to moderation_news_index_url
