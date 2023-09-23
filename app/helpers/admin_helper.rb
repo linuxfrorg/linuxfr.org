e96b9e01385c0ac2280273430a40064602969d00
echarp <manu@echarp.org>
2023-09-23 16:53:35 +0200

    Dépréciation de méthode File.exists?
    La méthode est maintenant nommée File.exist?

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,17 +1,14 @@
-# encoding: utf-8
 module AdminHelper
-
   def abusers
-    file = Rails.root.join("tmp", "abusers.txt")
-    if File.exists?(file)
-      content_tag(:ul, class: "abusers") do
+    file = Rails.root.join('tmp/abusers.txt')
+    if File.exist?(file)
+      content_tag(:ul, class: 'abusers') do
         File.readlines(file).map do |l|
           content_tag(:li, l)
         end.join.html_safe
       end
     else
-      content_tag(:p, "aucun")
+      content_tag(:p, 'aucun')
     end
   end
-
 end
