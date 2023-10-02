326a2e8ef1d93b0fcd789205845b62b4e1479466
echarp <manu@echarp.org>
2023-10-02 11:35:46 +0200

    Tous les contrôleurs admin sont testés, à 100%

---
+++ test/controllers/admin/maintainers_controller_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class Admin::MaintainersControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    sign_in accounts 'admin_0'
+    @account = accounts 'visitor_0'
+  end
+
+  test 'should give maintainer rights' do
+    post admin_account_maintainer_url @account
+    assert_redirected_to @account.user
+    assert_equal 'Nouveau rôle : mainteneur', flash[:notice]
+    assert @account.reload.maintainer?
+  end
+
+  test 'should remove maintainer rights' do
+    @account.give_maintainer_rights!
+    delete admin_account_maintainer_url @account
+    assert_redirected_to @account.user
+    assert_equal 'Rôle retiré : mainteneur', flash[:notice]
+    assert_not @account.reload.maintainer?
+  end
+end
+
---
+++ test/controllers/admin/moderators_controller_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class Admin::ModeratorsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    sign_in accounts 'admin_0'
+    @account = accounts 'visitor_0'
+  end
+
+  test 'should give moderator rights' do
+    post admin_account_moderator_url @account
+    assert_redirected_to @account.user
+    assert_equal 'Nouveau rôle : modérateur', flash[:notice]
+    assert @account.reload.moderator?
+  end
+
+  test 'should remove moderator rights' do
+    @account.give_moderator_rights!
+    delete admin_account_moderator_url @account
+    assert_redirected_to @account.user
+    assert_equal 'Rôle retiré : modérateur', flash[:notice]
+    assert_not @account.reload.moderator?
+  end
+end
