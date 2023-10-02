d9c99dd99d8c8618c7338e8e3d469cc5c79cf4d0
echarp <manu@echarp.org>
2023-10-02 11:35:46 +0200

    Tous les contrôleurs admin sont testés, à 100%

---
+++ test/controllers/admin/stylesheets_controller_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class Admin::StylesheetsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    sign_in accounts 'admin_0'
+  end
+
+  test 'should show stylesheet' do
+    get admin_stylesheet_url
+    assert_response :success
+  end
+
+  test 'should create stylesheet' do
+    # Tant qu'il manque un phantomjs de test
+    assert_raises NoMethodError do
+      post admin_stylesheet_url, params: {
+        url: 'https://linuxfr.org/test'
+      }
+    end
+  end
+end
