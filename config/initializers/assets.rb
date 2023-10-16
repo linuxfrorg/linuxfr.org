4dfa299ffcde17f5b9d9a1abe306988ecbce27c6
echarp <manu@echarp.org>
2023-10-16 19:38:17 +0200

    Suppression de références à node_modules

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,6 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
-Rails.application.config.assets.paths << Rails.root.join("node_modules")
 
 # SVG images
 Rails.application.config.assets.precompile += %w[feather-icons/dist/icons/arrow-left.svg]
